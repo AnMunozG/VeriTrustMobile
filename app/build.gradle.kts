4dea177691b26e53f5f8a10a92bd5bf9492b2f76
dilan <di.llanquileo@duocuc.cl>
2025-11-26 10:45:49 -0300

    j

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,12 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Retrofit y Gson
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("androidx.compose.material3:material3:1.5.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
+
 }
